062e384865a89362a236c9a875be01a99652fc9b
AtharvGaur <atharava112@gmail.com>
2024-12-26 16:59:08 +0530

    added tailewind

---
+++ OLX/Frontend/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import logo from '../../public/image 1.png'
+
+function Navbar() {
+  return (
+    <div className='p-1 flex'>
+      <div className="logo">
+        <img src={logo} alt="" />
+      </div>
+      <div>
+        <input type="" />
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
